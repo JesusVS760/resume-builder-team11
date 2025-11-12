fe661e41a27644ac03b0a01b6be3bf4b9596c9b9
Jesus Vazquez <tonio4340406@gmail.com>
2025-11-12 13:45:35 -0800

    TailoredResumeDAO setup

---
+++ src/dao/TailoredResumeDAO.java
@@ -0,0 +1,4 @@
+package dao;
+// NEED TO SAVE RESUMES AFTER TAILORED
+public class TailoredResumeDAO {
+}
--- src/services/ExportService.java
+++ src/services/ExportService.java
@@ -1,3 +1,11 @@
 // TODO: Implement ExportService class
 // This class handles exporting resume to PDF/DOCX
 // Methods: exportToPDF(), exportToDOCX(), generatePreview()
+
+public class ExportService {
+
+
+
+
+
+}
